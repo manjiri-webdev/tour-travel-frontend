e8666b41993d51df259293dd2fe9101ed47014e5
Vaidi Bhimani <miss.vaidi24@email.com>
2026-06-11 14:40:19 +0530

    added input validation in post api

--- server/api/bookings/create.post.ts
+++ server/api/bookings/create.post.ts
@@ -5,6 +5,28 @@ export default defineEventHandler(async (event) => {
 
   const { pickup_location, travel_date, mobile_number } = body;
 
+ // Date validation
+  const selectedDate = new Date(travel_date);
+  const today = new Date();
+
+  today.setHours(0, 0, 0, 0);
+  selectedDate.setHours(0, 0, 0, 0);
+
+  if (selectedDate < today) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Travel date cannot be in the past",
+    });
+  }
+
+  // Mobile validation
+  if (mobile_number.length !== 10 || isNaN(Number(mobile_number)) ){
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Mobile number must be exactly 10 digits",
+    });
+  }
+
   const result = await pool.query(
     `
       INSERT INTO bookings
